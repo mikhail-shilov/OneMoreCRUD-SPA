0af8b1c199ad585bc9b4f5597b38aa5eb79813e3
Mikhail Shilov <mih.shilov@gmail.com>
2020-08-01 19:50:12 +0300

    Fix. Add root src.

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import Filter from "./components/Filter";
+import DatasetMenu from "./components/DatasetMenu";
+import Table from "./components/table/Table";
+import FullData from "./components/FullData";
+
+function App(props) {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {(props.datasetType)
+                    ? <Filter
+                        filterState={props.filter}
+                        updateDraft={props.updateDraft}
+                        setFilter={props.setFilter}
+                    />: "Please select dataset for initializattion..." }
+            </header>
+            <main className="App-main">
+                {!(props.datasetType)
+                    ? <DatasetMenu
+                        isFetching={props.isFetching}
+                        getDataset={props.getDataset}/>
+                    : <Table
+                        tableColumns={props.tableColumns}
+                        sortMode={props.sortMode}
+                        sortDirection={props.sortDirection}
+                        activeFilter=''
+                        itemsPerPage={props.itemsPerPage}
+                        currentPage={props.currentPage}
+                        tableData={props.data}
+                        sortTable={props.setSortMode}
+                        setCurrentPage={props.setCurrentPage}
+                        setUserCard={props.setUserCard}
+                        editor={props.editor}
+                        switchEditor={props.switchEditor}
+                        updateEditor={props.updateEditor}
+                        insertToDataset={props.insertToDataset}
+                    />}
+
+
+            </main>
+            <footer className="App-footer">
+                {(props.datasetType)? <FullData userCard={props.userCard}/>: null }
+
+            </footer>
+        </div>
+    );
+}
+
+export default App;
